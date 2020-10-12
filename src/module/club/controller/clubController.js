90756b40eee83608c4f8afededbaeee354bc3763
juan <juancraft12yt@gmail.com>
2020-10-11 23:04:42 -0300

    Adds club-controller module
    Adds club controller module, interacts with club service module

---
+++ src/module/club/controller/clubController.js
@@ -0,0 +1,57 @@
+const abstractController = require("./abstractController.js")
+
+module.exports = class clubController extends abstractController{
+    /**
+     * 
+     * @param {import("../service/clubService.js")} clubService 
+     */
+    constructor(uploadMiddleware, clubService){
+        super()
+        this.ROUTE_BASE = "/club"
+        this.uploadMiddleware = uploadMiddleware
+        this.clubService = clubService
+    }
+
+    /**
+     * 
+     * @param {import("express").Application} app 
+     */
+    configureRoutes(app){
+        const ROUTE_BASE = this.ROUTE_BASE
+
+        app.get(`${ROUTE_BASE}`, this.index.bind(this))
+        app.get(`${ROUTE_BASE}ver-equipo?:id`, this.view.bind(this))
+    }
+
+    /**
+     * 
+     * @param {import("express").Response} res 
+     * @param {import("express").Request} req 
+     */
+    async index(req , res){
+        const teamList = this.clubService.getAll()
+
+        res.render("main", {
+            layout: "layout",
+            data:{
+                teamList
+            }
+        })
+    }
+
+
+    async view(){
+        const { id } = req.query.id
+
+        const team = this.clubService.getById(id)
+
+        res.render("view-team", {
+            layout: "layout",
+            data:{
+                team
+            }
+        })
+
+    }
+
+}
