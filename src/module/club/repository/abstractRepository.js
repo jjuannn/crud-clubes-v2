4d034bac31415de5411663e8742eefa8d935d9f8
juan <juancraft12yt@gmail.com>
2020-10-11 23:03:26 -0300

    Add abstractRepository

--- src/module/club/repository/abstractRepository.js
+++ src/module/club/repository/abstractRepository.js
@@ -6,20 +6,27 @@ class AbstractRepository{
     }
 
     /**
-     * @param {import("../entities/club.js")} equipo
+     * @param {import("../entities/club.js")} newTeam
      * @returns {import("../entities/club.js")}
      */
-    async save(equipo){}
+    async saveNewTeam(newTeam){}
 
+    /**
+     * @param {import("../entities/club.js")} editedTeam
+     * @returns {import("../entities/club.js")}
+     */
+    async saveEditedTeam(editedTeam){}
 
     /**
      * @param {Number} id
+     * @returns {Boolean}
      */
     async delete(id){}
 
     /**
      * 
      * @param {Number} id 
+     * @returns {import("../entities/club.js")}
      */
     async getById(id){}
 
