80f6389e12d1d4fb1c36a0562fa2bcb5f7a67bdc
juan <juancraft12yt@gmail.com>
2020-10-11 23:04:55 -0300

    Update di.js

--- src/config/di.js
+++ src/config/di.js
@@ -2,8 +2,9 @@ const uuid = require("uuidv4")
 const fs = require("fs")
 const { default: DIContainer, object, get, factory } = require("rsdi")
 const ClubRepository = require("../module/club/repository/json/clubRepository.js")
-const path = require("path")
-
+const ClubService = require("../module/club/service/clubService.js")
+const ClubController = require("../module/club/controller/clubController.js")
+const multer = require("multer")
 /**
  * @returns {Function}
  */
@@ -12,20 +13,37 @@ function configureUUID(){
 }
 
 function configureJSON_DB(){
-    process.env.JSON_DB_PATH = "./data/equipos.json"
+    process.env.JSON_DB_PATH = "./data/equipos.json" // ARREGLAR ESTO, NO  TIENE QUE ESTAR HARDCODEADO
     return process.env.JSON_DB_PATH
 }
+function configureMulter(){
 
+    const upload = multer({
+        dest: './uploads/imagenes'
+    })
+
+    return multer({ upload})
+}
+/**
+ * 
+ * @param {DIContainer} container 
+ */
 function addCommonDefinitions(container){
     container.addDefinitions({
         fs,
         uuid: factory(configureUUID),
-        JSON_DB_PATH: factory(configureJSON_DB)
+        JSON_DB_PATH: factory(configureJSON_DB),
+        multer: factory(configureMulter)
     })
 }
-
+/**
+ * 
+ * @param {DIContainer} container 
+ */
 function addClubModuleDefinitions(container){
     container.addDefinitions({
+        ClubController: object(ClubController).construct(get("multer"), get("ClubService")),
+        ClubService: object(ClubService).construct(get('ClubRepository')),
         ClubRepository: object(ClubRepository).construct(get("uuid"), get("fs"), get("JSON_DB_PATH"))
     })
 }
