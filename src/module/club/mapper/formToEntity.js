4220bbd0c4b27f572721411d31bab828f195d6b9
juan <juancraft12yt@gmail.com>
2020-10-11 23:03:49 -0300

    add form to entity mapper
    Adds a form-to-entity mapper

---
+++ src/module/club/mapper/formToEntity.js
@@ -0,0 +1,32 @@
+const { Equipo } = require("../entities/club")
+
+function formToEntity(team){
+    const {
+        nombre,
+        abreviatura,
+        estadio,
+        direccion,
+        anoFundacion,
+        numeroId,
+        telefono,
+        website,
+        pais,
+        fotoEscudo
+    } = team
+
+    return new Equipo(
+        nombre,
+        abreviatura,
+        estadio,
+        direccion,
+        anoFundacion,
+        numeroId,
+        telefono,
+        website,
+        pais,
+        fotoEscudo
+    )
+
+}
+
+module.exports = { formToEntity }
