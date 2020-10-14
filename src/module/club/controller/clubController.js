21c7128fcd46b4eca0173416f2992c0ae3649183
juan <juancraft12yt@gmail.com>
2020-10-13 23:25:59 -0300

    adds new methods
    adds functions for edit and save team info, and configure routes

--- src/module/club/controller/clubController.js
+++ src/module/club/controller/clubController.js
@@ -1,14 +1,16 @@
 const abstractController = require("./abstractController.js")
 
+
 module.exports = class ClubController extends abstractController{
     /**
      * 
      * @param {import("../service/clubService.js")} clubService 
      */
-    constructor(uploadMiddleware, clubService){
+    constructor(uploadMiddleware, bodyParser, clubService){
         super()
         this.ROUTE_BASE = "/club" 
         this.uploadMiddleware = uploadMiddleware
+        this.bodyParser = bodyParser
         this.clubService = clubService
     }
 
@@ -20,7 +22,41 @@ module.exports = class ClubController extends abstractController{
         const ROUTE_BASE = this.ROUTE_BASE
 
         app.get(`${ROUTE_BASE}`, this.index.bind(this))
-        app.get(`${ROUTE_BASE}ver-equipo?:id`, this.view.bind(this))
+        app.get(`${ROUTE_BASE}/ver-equipo?:id`, this.view.bind(this))
+        app.get(`${ROUTE_BASE}/editar-equipo?:id`, this.edit.bind(this))
+        app.post(`${ROUTE_BASE}/editar-equipo?:id`, this.bodyParser , this.saveEditedTeam.bind(this))
+    }
+    
+    /**
+     * 
+     * @param {import("express").Response} res 
+     * @param {import("express").Request} req
+     */
+    async saveEditedTeam(req, res){
+
+        const editedTeam = req.body
+        console.log(req.body)
+        this.clubService.saveEditedTeam(editedTeam)
+
+        return res.redirect("/")
+    }
+
+    /**
+     * 
+     * @param {import("express").Response} res 
+     * @param {import("express").Request} req 
+     */
+    async edit(req, res){
+        const id = req.query.id
+
+        const equipo = await this.clubService.getById(id)
+
+        res.render("edit-team", {
+            layout: "layout",
+            data:{
+                equipo
+            }
+        })
     }
 
     /**
@@ -39,11 +75,15 @@ module.exports = class ClubController extends abstractController{
         })
     }
 
+    /**
+     * 
+     * @param {import("express").Response} res 
+     * @param {import("express").Request} req 
+     */
+    async view(req, res){
+        const id = req.query.id
 
-    async view(){
-        const { id } = req.query.id
-
-        const team = this.clubService.getById(id)
+        const team = await this.clubService.getById(id)
 
         res.render("view-team", {
             layout: "layout",
@@ -53,5 +93,7 @@ module.exports = class ClubController extends abstractController{
         })
 
     }
+
+
 
 }
