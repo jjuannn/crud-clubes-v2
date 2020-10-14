21a677635d3d81845aca21c5c0ceadf9f97f9a43
juan <juancraft12yt@gmail.com>
2020-10-13 23:25:14 -0300

    adds body-parser middleware
    and it is used in the constructor of ClubController

--- src/config/di.js
+++ src/config/di.js
@@ -1,17 +1,14 @@
-const uuid = require("uuidv4")
 const fs = require("fs")
 const { default: DIContainer, object, get, factory } = require("rsdi")
 const { ClubController, ClubService, ClubRepository} = require("../module/module");
-
-
+const bodyParser = require("body-parser")
 const multer = require("multer")
-/**
- * @returns {Function}
- */
-function configureUUID(){
-    return uuid
-}
 
+function configureBodyParser(){
+    const urlencodedParser = bodyParser.urlencoded({ extended: false })
+    
+    return urlencodedParser
+}
 function configureJSONDB(){
     return process.env.JSON_DB_PATH
 }
@@ -30,20 +27,21 @@ function configureMulter(){
 function addCommonDefinitions(container){
     container.addDefinitions({
         fs,
-        uuid: factory(configureUUID),
+        bodyParser: factory(configureBodyParser),
         JSON_DB_PATH: factory(configureJSONDB),
         multer: factory(configureMulter)
     })
 }
+
 /**
  * 
  * @param {DIContainer} container 
  */
 function addClubModuleDefinitions(container){
     container.addDefinitions({
-        ClubController: object(ClubController).construct(get("multer"), get("ClubService")),
+        ClubController: object(ClubController).construct(get("multer"), get("bodyParser"), get("ClubService")),
         ClubService: object(ClubService).construct(get('ClubRepository')),
-        ClubRepository: object(ClubRepository).construct(get("uuid"), get("fs"), get("JSON_DB_PATH"))
+        ClubRepository: object(ClubRepository).construct(get("fs"), get("JSON_DB_PATH"))
     })
 }
 /**
