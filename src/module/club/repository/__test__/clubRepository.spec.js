3daad0bbd7941e4c674b936cc06356ee7fde8ac1
juan <juancraft12yt@gmail.com>
2020-10-24 21:31:11 -0300

    Update clubRepository.spec.js

--- src/module/club/repository/__test__/clubRepository.spec.js
+++ src/module/club/repository/__test__/clubRepository.spec.js
@@ -1,5 +1,4 @@
 const { Equipo } = require("../../entities/club")
-const { mapearDB } = require("../../mapper/dbMapper")
 const IdInUseError = require("../errors/idInUseError")
 const IdNotFoundError = require("../errors/idNotFoundError")
 const InvalidIdError = require("../errors/invalidIdError")
@@ -68,4 +67,41 @@ test("prueba correctamente la function getById", async() => {
     expect(repository.getAll).toHaveBeenCalledTimes(1)
     expect(selectedTeam).toBeInstanceOf(Equipo)
     expect(Array.prototype.findIndex).toHaveBeenCalledTimes(1)
+})
+test("prueba guardar un nuevo equipo correctamente", async() => {
+    const newTeam = new Equipo()
+    Array.prototype.find = jest.fn().mockImplementationOnce(() => false)
+    Array.prototype.push = jest.fn().mockImplementationOnce(() => [{}])
+    repository.getAll = jest.fn().mockImplementationOnce(() => [])
+
+    await repository.saveNewTeam(newTeam)
+
+    expect(Array.prototype.find).toHaveBeenCalledTimes(1)
+    expect(Array.prototype.push).toHaveBeenCalledTimes(1)
+    expect(repository.writeDb).toHaveBeenCalledTimes(1)
+})
+
+test("falla al intentar guardar un nuevo equipo con un ID en uso", async() => {
+    const newTeam = new Equipo()
+    Array.prototype.find = jest.fn().mockImplementationOnce(() => true)
+    repository.getAll = jest.fn().mockImplementationOnce(() => [])
+
+    try{
+        await repository.saveNewTeam(newTeam)
+    }catch(e){
+        expect(e).toBeInstanceOf(IdInUseError) // corregir este test
+    }
+
+})
+
+test("guarda correctamente la edicion de los equipos", async() => {
+    repository.getAll = jest.fn().mockImplementationOnce(() => [{name:"team", id: "3", fotoEscudo: "/fake_route/12345"}])
+    Array.prototype.findIndex = jest.fn().mockImplementationOnce(() => "0")
+    //Array.prototype.splice = jest.fn().mockImplementationOnce(() => )
+    const editedTeam = {name:"team2", id: "3"}
+    
+    await repository.saveEditedTeam(editedTeam)
+
+    expect(Array.prototype.splice).toHaveBeenCalledTimes(41238912)
+
 })
